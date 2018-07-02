17025d2c4c278c4f2530db32ed22a96ee035c466
Joshua Ondieki <joshuaondieki8@gmail.com>
2018-07-02 15:30:28 +0300

    Add app interface

--- ui.py
+++ ui.py
@@ -1,3 +1,23 @@
 """
-    APP UI
+    APP ui
 """
+
+from termcolor import colored
+
+
+def success(text):
+    """Returns a green colored text
+    """
+    return(colored(text, 'green', attrs=['bold']))
+
+
+def magenta(text):
+    """Returns a magenta colored text
+    """
+    return(colored(text, 'magenta', attrs=['blink', 'bold']))
+
+
+def error(text):
+    """Returns a red colored text
+    """
+    return(colored(text, 'red', attrs=['bold']))
