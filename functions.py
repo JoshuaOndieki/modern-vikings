79d06f5d47eabf08d4941f9003816747e885fba2
Abdulaziz Mohammed <abdulmoha786@gmail.com>
2018-07-02 15:31:53 +0300

    user class and comment functions

--- functions.py
+++ functions.py
@@ -1,3 +1,21 @@
 """
     FUNCTIONS
 """
+from models.comment import Comment
+from models.user import User
+
+class Functions(object):
+    logged_in_user = ""
+
+
+    def create_comment(self, parent, comment1):
+        for message in Comment.messages:
+            if message['author'] == Functions.logged_in_user:
+                comment1.parent = parent
+                Comment.messages.append(comment1)
+
+    def delete_comment(self, comment_id):
+        pass
+
+
+
