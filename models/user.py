be05fa54b8b566739be065e58e7ff9445892031b
SelaDanti <kwameasiago@gmail.com>
2018-07-02 15:30:49 +0300

    Modify user

--- models/user.py
+++ models/user.py
@@ -1,31 +1,47 @@
-class user_data:
-	register_data = []
+class user:
 	"""
-	check if data entry is empty
+	function adds users
 	"""
-	def signup_empty(self,user_type,user_name,password):
-		pass
+	def add_user(self,user_type,username,password,user_data):
+		item = {
+		'user_type':user_type,
+		'username': username,
+		'password':password,
+		'id':len(user_data)
+		}
+		user_data.append(item)
+		return({'result':'Account created'})
 
-	def signup_whitespace(self,user_type,user_name,password):
-		pass
+	"""
+	function gets all user
+	"""
+	def get_all(self,user_data):
+		return(user_data)
 
-	def user_type(self,user_type):
-		pass
+	"""
+	function to get one user(if user exist)
+	"""
+	def get_one(self,username,user_data):
+		items = user_data
+		for item in items:
+			if item['username'] == username:
+				return(item)
+			else:
+				return({'result':'username does not exist'})
 
-	def user_exist(self,user_name):
-		pass
+	def get_userType(self,username,user_data):
+		items = user_data
+		for item in items:
+			if item['username'] == username:
+				return({'result':item['user_type']})
+			else:
+				return({'result':'username does not exist'})
 
-	def register_user(self,user_type,user_name,password):
-		pass
-
-
-class signup_user(user_data):
-	def __init__(self,user_type,user_name,password):
-		self.user_name = user_name
-		self.user_type = user_type
-		self.password = password
-
-
-
-
+	def login_check(self,password,username,user_data):
+		items = user_data
+		for item in items:
+			if item['username'] == username and item['password'] == password:
+				return ({'result':True})
+			else:
+				return({'result':False})
 
